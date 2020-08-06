a78f992e3d39b9ce2bf5f6b2b6ec49c6a73d8cbc
Monika Swidzinska <monikaswidzinska@Monikas-MacBook-Pro.local>
2020-08-06 16:51:00 +0200

    logic-not finished

--- FizzBuzzGame/src/app/app.component.ts
+++ FizzBuzzGame/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FizzBuzzService} from "../services/fizzBuzz.service";
+import {startWith} from "rxjs/operators";
+
 
 @Component({
   selector: 'app-root',
@@ -8,24 +10,64 @@ import {FizzBuzzService} from "../services/fizzBuzz.service";
 })
 export class AppComponent implements OnInit {
   title = 'FizzBuzzGame';
-  gameArray: number | string;
+  fbVal: number | string; //FizzBuzz Values
+  clickVal: string; // clicked Values
+  score = 0;
 
   constructor(protected fizzBuzzService: FizzBuzzService) {
   }
 
   ngOnInit(): void {
-
   }
 
-  onStartClick(): any {
+  onStartClick(): void {
     this.game();
   }
 
+  onFizzClick(): void {
+    this.clickVal = 'Fizz';
+    this.clear();
+  }
+
+  onBuzzClick(): void {
+    this.clickVal = 'Buzz';
+    this.clear();
+  }
+
+  onFizzBuzzClick(): void {
+    this.clickVal = 'FizzBuzz';
+    this.clear();
+  }
+
+  clear(): void {
+    setTimeout(() => {
+      this.clickVal = '';
+    }, 2990);
+  }
+
   game(): any {
-    this.fizzBuzzService.fizzBuzz().subscribe((response) => {
-      this.gameArray = response;
-      console.log(this.gameArray)
-    });
+    this.fizzBuzzService.fizzBuzz()
+      .pipe(startWith("GO!"))
+      .subscribe((response) => {
+        this.fbVal = response
+        if (this.clickVal == this.fbVal) {
+          this.score += 1
+        }
+        else if (this.clickVal && this.clickVal !== this.fbVal) {
+          this.score -= 1
+        }
+        else if (this.fbVal && !this.clickVal) {
+          this.score +=0;
+        }
+        else if (this.score){
+          this.score +=100;
+        }
+
+
+        console.log('clickVal: ', this.clickVal);
+        console.log('fbVal: ', this.fbVal);
+        console.log('score: ', this.score);
+      });
   }
 }
 
--- FizzBuzzGame/src/services/fizzBuzz.service.ts
+++ FizzBuzzGame/src/services/fizzBuzz.service.ts
@@ -1,26 +1,26 @@
 import {Injectable} from '@angular/core';
-import {zip, interval, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {zip, Observable, timer} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FizzBuzzService {
-  counter$: Observable<number> = interval(1000).pipe
+  numbers$: Observable<number> = timer(1000, 3000).pipe
   (map(n => n += 1));
 
-  fizz$: Observable<string> = this.counter$.pipe
+  fizz$: Observable<string> = this.numbers$.pipe
   (map(n => n % 3 === 0 ? 'Fizz' : null));
 
-  buzz$: Observable<string> = this.counter$.pipe
+  buzz$: Observable<string> = this.numbers$.pipe
   (map(n => n % 5 === 0 ? 'Buzz' : null));
 
   fizzBuzz(): Observable<number | string> {
-    return zip(this.counter$, this.fizz$, this.buzz$)
+    return zip(this.numbers$, this.fizz$, this.buzz$)
       .pipe(
         map(
-          ([counter$, fizz$, buzz$]) =>
-            ([fizz$ == null && buzz$ == null ? counter$ : null,
+          ([numbers$, fizz$, buzz$]) =>
+            ([fizz$ == null && buzz$ == null ? numbers$ : null,
               fizz$,
               buzz$])
               .filter((v) => v !== null).join('')
@@ -28,32 +28,3 @@ export class FizzBuzzService {
       )
   }
 }
-
-
-// getBreweries(): Observable<BreweryDetails> {
-//   return this.http.get(`api/breweries?withLocations=Y&${environment.KEY}`)
-//     .pipe(map((response: ApiResponse) => response.data as BreweryDetails));
-// }
-
-// const counter$: Observable<number> = interval(1000).pipe
-// (map(n => n += 1));
-//
-// const fizz$: Observable<string> = counter$.pipe
-// (map(n => n % 3 === 0 ? 'Fizz' : null));
-//
-// const buzz$: Observable<string> = counter$.pipe
-// (map(n => n % 5 === 0 ? 'Buzz' : null));
-//
-// const fizzBuzz$ = zip(counter$, fizz$, buzz$)
-//   .pipe(
-//     map(
-//       ([counter$, fizz$, buzz$]) =>
-//         ([fizz$ == null && buzz$ == null ? counter$ : null,
-//           fizz$,
-//           buzz$])
-//           .filter((v) => v !== null).join('')
-//     )
-//   )
-// fizzBuzz$.subscribe(v => {
-//   console.log(v);
-// });
