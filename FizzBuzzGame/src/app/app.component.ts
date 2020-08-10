fd776aea8544db7d433e7ce1bac45727911530bb
Monika Swidzinska <monikaswidzinska@Monikas-MacBook-Pro.local>
2020-08-10 13:00:36 +0200

    game shows numbers, number button

--- FizzBuzzGame/src/app/app.component.ts
+++ FizzBuzzGame/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FizzBuzzService} from "../services/fizzBuzz.service";
 import {startWith} from "rxjs/operators";
 import {isNumeric} from "rxjs/internal-compatibility";
-import {Subject} from "rxjs";
+import {Observable, Subject} from "rxjs";
 
 
 @Component({
@@ -13,52 +13,85 @@ import {Subject} from "rxjs";
 export class AppComponent implements OnInit {
   title = 'FizzBuzzGame';
   game: string; //FizzBuzz Values
+  numbers: number;
   clickVal: string; // clicked Values
   score = 0;
-  public fizzBuzzSubject = new Subject();
+  public onStartClick = new Subject<boolean>();
   public onButtonClick = new Subject<string>();
 
+
   constructor(protected fizzBuzzService: FizzBuzzService) {
+
   }
 
-  ngOnInit(): void {}
-
-  onStartClick(): void {
+  ngOnInit(): void {
     this.playGame();
   }
 
-  playGame() {
-    this.fizzBuzzSubject.next(
-      this.fizzBuzzService.fizzBuzz()
-        .pipe(
-          startWith("GO!"))
-        .subscribe((response) => {
-          this.game = response;
+  // onStartClick(): void {
+  //   this.playGame();
+  // }
 
-          this.clickVal == this.game ? this.score += 1 :                     // proper button clicked: +1 point
-            ((this.clickVal && this.clickVal !== this.game) ||              // if you clicked, but wrong button or
-              ((isNumeric(this.game) === false && this.game !== "GO!")     // there is a word in game (Fizz, Buzz, FizzBuzz, GO!)
-                && (this.clickVal == '' || this.clickVal === undefined))) // and you didn't click anything when you supposed to
-              ? this.score -= 1 :                                        // then:-1 point
-              (this.game && !this.clickVal)                             // if the game is on but you didn't click anything (condition for numbers)
-                ? this.score += 0 : null;                              // do nothing with 'score'
+  //playGame() {
+    // this.fizzBuzzService.fizzBuzz()
+    //   .pipe(
+    //     startWith("GO!"))
+    //   .subscribe((response) => {
+    //     this.game = response
+    //     this.clickVal == this.game ? this.score += 1 :                     // proper button clicked: +1 point
+    //       ((this.clickVal && this.clickVal !== this.game) ||              // if you clicked, but wrong button or
+    //         ((isNumeric(this.game) === false && this.game !== "GO!")     // there is a word in game (Fizz, Buzz, FizzBuzz, GO!)
+    //           && (this.clickVal == '' || this.clickVal === undefined))) // and you didn't click anything when you supposed to
+    //         ? this.score -= 1 :                                        // then:-1 point
+    //         (this.game && !this.clickVal)                             // if the game is ON but you didn't click anything and there is a number
+    //           ? this.score += 0 : null;                              // do nothing with 'score'
+    //
+    //     this.score === -2 ? this.reset() : null;                    // if you reach -5 points = Game Over! and reset te game
+    //
+    //     // console.log('clickVal: ', this.clickVal);
+    //     // console.log('game: ', this.game);
+    //     // console.log('score: ', this.score);
+    //   });
+  //}
+    playGame() {
+      this.onStartClick.subscribe((response)=>{
+        this.fizzBuzzService.numbersStream$.subscribe((val =>
+          this.numbers = val))
+        this.fizzBuzzService.fizzBuzz()
+          .pipe(
+            startWith("GO!"))
+          .subscribe((response) => {
+            this.game = response
+            this.clickVal == this.game ? this.score += 1 :                     // proper button clicked: +1 point
+              ((this.clickVal && this.clickVal !== this.game) ||              // if you clicked, but wrong button or
+                ((isNumeric(this.game) === false && this.game !== "GO!")     // there is a word in game (Fizz, Buzz, FizzBuzz, GO!)
+                  && (this.clickVal == '' || this.clickVal === undefined))) // and you didn't click anything when you supposed to
+                ? this.score -= 1 :                                        // then:-1 point
+                (this.game && !this.clickVal)                             // if the game is ON but you didn't click anything and there is a number
+                  ? this.score += 0 : null;                              // do nothing with 'score'
 
-          this.score === -1 ? this.reset() : null;                    // if you reach -5 points = Game Over! and reset te game
+            this.score === -2 ? this.reset() : null;                    // if you reach -5 points = Game Over! and reset te game
+            //
+            // console.log('game: ', this.game);
+            // console.log('clickVal: ', this.clickVal);
+            // console.log('score: ', this.score);
+          })
 
-          console.log('clickVal: ', this.clickVal);
-          console.log('game: ', this.game);
-          console.log('score: ', this.score);
-        }));
+
+        console.log('onStartClick.subscribe',response)
+      })
+
     this.onButtonClick.subscribe((response)=>{
       this.clickVal = response;
       this.clear();
+      console.log('onButtonClick.subscribe',response)
     })
   }
 
   clear(): void {
     setTimeout(() => {
       this.clickVal = '';
-    }, 2990);
+    }, 1000);
   }
 
   reset() {
@@ -70,10 +103,9 @@ export class AppComponent implements OnInit {
 
 
 // TO DO:
-//DONE// make start button visible after reset
-//DONE// make GO! visible again
-//DONE// click in Rx JS?
-// make if ? : else in a  better   way?
-// async? for measuring points (change an order)
-// better alert
+// async? for measuring points (change an order) - different method in Rx JS, fromEvent
+// make if ? : else in a  better way?
 //write better rules
+// fromEvent('click', [Your Element]).pipe(
+//   map((event) => event.target.value)
+// )
--- FizzBuzzGame/src/services/fizzBuzz.service.ts
+++ FizzBuzzGame/src/services/fizzBuzz.service.ts
@@ -1,13 +1,17 @@
 import {Injectable} from '@angular/core';
-import {zip, Observable, timer, Subject} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {zip, Observable, timer} from 'rxjs';
+import {map, share} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FizzBuzzService {
-  numbers$: Observable<number> = timer(1000, 3000).pipe
+
+  numbers$: Observable<number> = timer(1000, 1000).pipe
   (map(n => n += 1));
+
+  public numbersStream$: Observable<number>= this.numbers$.pipe(share())
 
   fizz$: Observable<string> = this.numbers$.pipe
   (map(n => n % 3 === 0 ? 'Fizz' : null));
@@ -16,6 +20,7 @@ export class FizzBuzzService {
   (map(n => n % 5 === 0 ? 'Buzz' : null));
 
   fizzBuzz(): Observable<string> {
+    this.numbersStream$ = this.numbers$.pipe(share())
     return zip(this.numbers$, this.fizz$, this.buzz$)
       .pipe(
         map(
